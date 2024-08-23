0e5ac48f77f856151ec2a9b75e59ceca6b58e1d6
ashraynarain <ashraynarain@gmail.com>
2024-08-24 02:38:23 +0530

    Initial Commit

---
+++ client/src/Post.js
@@ -0,0 +1,18 @@
+export default function Post(){
+    return (
+        <div className="post">
+        <div className="image">
+        <img src="https://techcrunch.com/wp-content/uploads/2024/08/in-brief-ai-v2.jpg?resize=680,383" alt =""></img>
+        </div>
+        <div className="texts">
+          <h2>DeepMind workers sign letter in protest of Google's defense contracts</h2>
+          <p className="info">
+            <a className="author">Ashray Narain</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">At least 200 workers at DeepMind, Google’s AI R&D division, are displeased with Google’s reported defense contracts — and according to Time, they circulated a letter internally back in May to say as much.</p>
+        </div>
+        
+      </div>
+    );
+}
